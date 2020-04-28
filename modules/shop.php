7cd4a2010e8542d3cfcb1365dda39900a712ce5f
Sam S <sam.kdt99@gmail.com>
2020-04-28 14:30:38 +0200

    fixat tid och recusive costumer mapp skapande

--- modules/shop.php
+++ modules/shop.php
@@ -12,27 +12,28 @@
 <?PHP
 
 	function folder() {
-		$date = date("d:y");
+		$date = date("F:Y");
 		$dir = "./customers/".$date;
 		if (!file_exists($dir)){
-			mkdir($dir);
+			mkdir($dir, 0777, true);
 		}
 		return $dir;
 	}
 	
 	function mkreceipt($name,$lname,$order,$email){
 		$ip =  $_SERVER['REMOTE_ADDR'];
+		$time = date("d/H:i");
 		$dir = folder();
 		$file = $dir."/orders.csv";
 		if (!file_exists($file)){
 			$receiptinit = fopen($file,"w") or die("something snea");
-			$categorys = "order,namn,berställning,epost,ip,status";
+			$categorys = "order,namn,berställning,epost,ip,tid,status";
 			fwrite($receiptinit, $categorys);
 			fclose($receiptinit);
 		}
 		$orderid = exec("cat $file | wc -l")+1;
 		$receipt = fopen($file, "a") or die("Unable to open file!");
-		$txt = "\n".$orderid.",".$name." ".$lname.",".$order . ",".$email."," . $ip.",pending";
+		$txt = "\n".$orderid.",".$name." ".$lname.",".$order . ",".$email."," . $ip.",".$time.",pending";
 	
 		fwrite($receipt, $txt);
 		fclose($receipt);
