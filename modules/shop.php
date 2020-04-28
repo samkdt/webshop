2ded0dbea536dd5a80c943a1fc6fd992bf3255f4
NeoIsRecursive <neo.leijondahl@gmail.com>
2020-04-28 15:48:59 +0200

    begränsatt beställningar

--- modules/shop.php
+++ modules/shop.php
@@ -3,21 +3,49 @@
 	firstName:	<input type="text" name="fname">
 	lastName:	<input type="text" name="lname">
 	email:		<input type="email" name="email">
-	order:		<input type="text" name="order">
+	storlek:	<select id="size" name="size">
+				<option value="small">small</option>
+				<option value="medium">medium</option>
+				<option value="large">large</option>
+			</select>	
 	<input type="submit">
 
 </form>
 
 
 <?PHP
-
+checkopd();
 	function folder() {
 		$date = date("F:Y");
-		$dir = "./customers/".$date;
+		$dir = "./orders/".$date;
 		if (!file_exists($dir)){
 			mkdir($dir, 0777, true);
 		}
 		return $dir;
+	}
+
+	function checkopd(){
+		$yeet = "./orders/opd.txt";
+		if (file_exists($yeet)){
+			if (exec(" wc -l < $yeet") > 4){
+				echo "DU KOM FÖRSENT, FÖRSÖk IGEN IMORGON";
+			}
+		} else {
+			$hej = fopen($yeet,"w") or die("nåt snea, du ör för smallbrain");
+			fwrite($hej,date("d/f"));
+			fclose($hej);
+		}
+
+	
+	}
+
+	function updateopd(){
+		
+		$yeet = "./orders/opd.txt";
+		$hej = fopen($yeet,"a") or die("nåt snea, du ör för smallbrain");
+		fwrite($hej,"\nenmindrekvar");
+		fclose($hej);
+
 	}
 	
 	function mkreceipt($name,$lname,$order,$email){
@@ -31,12 +59,13 @@
 			fwrite($receiptinit, $categorys);
 			fclose($receiptinit);
 		}
-		$orderid = exec("cat $file | wc -l")+1;
+		$orderid = exec("wc -l < $file")+1;
 		$receipt = fopen($file, "a") or die("Unable to open file!");
 		$txt = "\n".$orderid.",".$name." ".$lname.",".$order . ",".$email."," . $ip.",".$time.",pending";
 	
 		fwrite($receipt, $txt);
 		fclose($receipt);
+		updateopd();
 		echo "Beställning gjord, kolla din epost för kvitto glöm inte betala";
 	}	
 
@@ -45,7 +74,7 @@
 		$name = $_POST["fname"];
 		$lname = $_POST["lname"];
 		$email = $_POST["email"];
-		$order = $_POST["order"];
+		$order = $_POST["size"];
 		mkreceipt($name,$lname,$order,$email);
 
 	}
